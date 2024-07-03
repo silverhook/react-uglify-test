0384fff7d1418c33749dcf95f5416f8e3e320796
Carlos <carlopolo@gmail.com>
2024-07-03 13:55:12 +0100

    first draft

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import SpecialComponent from './SpecialComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SpecialComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/SpecialComponent.js
@@ -0,0 +1,17 @@
+/*!
+ * This is a special comment that will be preserved.
+ * Author: Your Name
+ * License: MIT
+ */
+import React from 'react';
+
+function SpecialComponent() {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    );
+}
+
+export default SpecialComponent;
+
